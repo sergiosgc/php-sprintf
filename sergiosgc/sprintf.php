1c10709a019613cb8d4a09dbcb058fef75ed2418
Sérgio Carvalho <sergio.carvalho@sooma.com>
2019-06-22 13:03:54 +0100

    Reimplement using regular expressions

--- sergiosgc/sprintf.php
+++ sergiosgc/sprintf.php
@@ -10,25 +10,22 @@ function sprintf($format) {
     $args = func_get_args();
     array_shift($args);
     if (count($args) && (is_array($args[count($args) - 1]) || $args[count($args) - 1] instanceof \ArrayAccess)) $namedArguments = array_pop($args); else $namedArguments = [];
-    $offset = 0;
-    $sprintfArgs = [];
-    while( ($convertionOffset = strpos($format, '%', $offset)) !== FALSE ) {
-        if ($convertionOffset == (strlen($format)-1)) break;
-        $offset = $convertionOffset+1;
-        if ($format[$offset] == '<') {
-            $nameEnd = strpos($format, '>', $offset);
-            if ($nameEnd === FALSE) throw new \Exception('Unterminated convertion specifier name: ' . substr($format, $offset));
-            $convertionName = substr($format, $offset+1, $nameEnd - $offset - 1);
-            $format = substr($format, 0, $offset) . substr($format, $nameEnd+1);
-            if (!isset($namedArguments[$convertionName])) throw new \Exception('Named convertion specifier not found: ' . $convertionName);
-            $sprintfArgs[] = $namedArguments[$convertionName];
-        } elseif ($format[$offset] == '%') {
-            $offset+=1;
+    if (!preg_match_all('_(?:(?<doublepercent>%%)|(?<preceding>.*?(?:^|[^%]))%<(?<var>[^>]*)>(?<succeeding>.*?)|(?<nonvar>[^%]*%?))_', $format, $matches, PREG_SET_ORDER)) throw new \Exception('Error matching format regex');
+    foreach ($matches as $i => $match) $matches[$i] = array_filter($match, "is_string", ARRAY_FILTER_USE_KEY);
+    $result = '';
+    foreach ($matches as $match) {
+        if (strlen($match['doublepercent'])) {
+            $result .= '%%';
+        } elseif (strlen($match['nonvar'])) {
+            $result .= $match['nonvar'];
+        } elseif (strlen($match['var'])) {
+            if (!isset($namedArguments[$match['var']])) throw new \Exception('Named convertion specifier not found: ' . $match['var']);
+            $result .= $match['preceding'] . $namedArguments[$match['var']] . $match['succeeding'];
         } else {
-            if (count($args) == 0) throw new \Exception('Unmatched convertion specifier');
-            $sprintfArgs[] = array_shift($args);
+            if (0 == strlen(implode('', $match))) continue;
+            throw new \Exception('Unexpected match in format');
         }
     }
-    array_unshift($sprintfArgs, $format);
-    return call_user_func_array('\sprintf', $sprintfArgs);
+    array_unshift($args, $result);
+    return \call_user_func_array('\sprintf', $args);
 }
